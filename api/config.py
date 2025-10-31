c1003e62e8faa091523e182f67c30df585e7252b
badman99dev <badman993944@gmail.com>
2025-10-31 14:41:33 +0530

    Create config.py

---
+++ api/config.py
@@ -0,0 +1,7 @@
+# api/config.py
+
+# Purana URL (Galat):
+# INVIDIOUS_API_BASE = "https://invidious.sethforprivacy.com/api/v1"
+
+# Naya URL (Sahi - Tumhare wala):
+INVIDIOUS_API_BASE = "https://inv.perditum.com/api/v1"
