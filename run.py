ed528ac0f318116f7c3e392b012ee247d0f9b6c9
badman99dev <badman993944@gmail.com>
2025-10-31 15:15:40 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 # run.py
 from flask import Flask
+from flask_cors import CORS  # <--- 1. Isko import karo
 from api.routes import api_blueprint
 
 def create_app():
     app = Flask(__name__)
+    
+    # --- 2. Is line ko add karo ---
+    # Iska matlab hai: Is app par kisi bhi origin se request aane do (*)
+    CORS(app) 
+    
     # Hamare API blueprint ko '/api' route par register karo
     app.register_blueprint(api_blueprint, url_prefix='/api')
     return app
