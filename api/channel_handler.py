b449cde5c0f33820e6a284bd721ffb6b34ed5f74
badman99dev <badman993944@gmail.com>
2025-11-01 16:39:08 +0530

    Delete api directory

--- api/channel_handler.py
+++
@@ -1,9 +0,0 @@
-# api/channel_handler.py
-from api.config import INVIDIOUS_API_BASE # <-- IMPORT UPDATED
-
-async def get_channel_details(session, channel_id):
-    """Fetches details for a channel (banner, videos, etc.)."""
-    url = f"{INVIDIOUS_API_BASE}/authors/{channel_id}"
-    async with session.get(url) as response:
-        response.raise_for_status()
-        return await response.json()
--- api/search_handler.py
+++
@@ -1,9 +0,0 @@
-# api/search_handler.py
-from api.config import INVIDIOUS_API_BASE # <-- IMPORT UPDATED
-
-async def perform_search(session, query):
-    """Performs a search query."""
-    url = f"{INVIDIOUS_API_BASE}/search?q={query}&type=video"
-    async with session.get(url) as response:
-        response.raise_for_status()
-        return await response.json()
